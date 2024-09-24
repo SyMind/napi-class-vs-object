192132d397d3935f6d386b2be98a79e928d53583
Cong-Cong <dacongsama@live.com>
2024-09-24 13:02:33 +0800

    feat: p90

--- bench.js
+++ bench.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const percentile = require('percentile');
 const {
     createObject1,
     createObject2,
@@ -30,218 +30,170 @@ const {
     setClass4,
 } = require(".");
 
-console.time("object with 1 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject1();
-}
-console.timeEnd("object with 1 field");
+console.log("object with 1 field", measure(() => {
+    const o = createObject1();
+}));
+console.log("object with 2 field", measure(() => {
+    const o = createObject2();
+}));
+console.log("object with 3 field", measure(() => {
+    const o = createObject3();
+}));
+console.log("object with 4 field", measure(() => {
+    const o = createObject4();
+}));
+console.log("object with 5 field", measure(() => {
+    const o = createObject5();
+}));
+console.log("object with 6 field", measure(() => {
+    const o = createObject6();
+}));
+console.log("object with 7 field", measure(() => {
+    const o = createObject7();
+}));
+console.log("object with 8 field", measure(() => {
+    const o = createObject8();
+}));
+console.log("object with 9 field", measure(() => {
+    const o = createObject9();
+}));
+console.log("object with 10 field", measure(() => {
+    const o = createObject10();
+}));
 
-console.time("object with 2 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject2();
-}
-console.timeEnd("object with 2 field");
 
-console.time("object with 3 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject3();
-}
-console.timeEnd("object with 3 field");
 
-console.time("object with 4 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject4();
+
+
+console.log("class with 1 field", measure(() => {
+    const o = createClass1();
+}));
+console.log("class with 2 field", measure(() => {
+    const o = createClass2();
+}));
+console.log("class with 3 field", measure(() => {
+    const o = createClass3();
+}));
+console.log("class with 4 field", measure(() => {
+    const o = createClass4();
+}));
+console.log("class with 5 field", measure(() => {
+    const o = createClass5();
+}));
+console.log("class with 6 field", measure(() => {
+    const o = createClass6();
+}));
+console.log("class with 7 field", measure(() => {
+    const o = createClass7();
+}));
+console.log("class with 8 field", measure(() => {
+    const o = createClass8();
+}));
+console.log("class with 9 field", measure(() => {
+    const o = createClass9();
+}));
+console.log("class with 10 field", measure(() => {
+    const o = createClass10();
+}));
+
+
+
+
+console.log("read all class with 1 field", measure(() => {
+    const o = createClass1();
     o.f1;
+}));
+console.log("read all class with 2 field", measure(() => {
+    const o = createClass2();
+    o.f1;
+    o.f2;
+}));
+console.log("read all class with 3 field", measure(() => {
+    const o = createClass3();
+    o.f1;
+    o.f2;
+    o.f3;
+}));
+console.log("read all class with 4 field", measure(() => {
+    const o = createClass4();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+}));
+console.log("read all class with 5 field", measure(() => {
+    const o = createClass5();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+}));
+console.log("read all class with 6 field", measure(() => {
+    const o = createClass6();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+    o.f6;
+}));
+console.log("read all class with 7 field", measure(() => {
+    const o = createClass7();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+    o.f6;
+    o.f7;
+}));
+console.log("read all class with 8 field", measure(() => {
+    const o = createClass8();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+    o.f6;
+    o.f7;
+    o.f8;
+}));
+console.log("read all class with 9 field", measure(() => {
+    const o = createClass9();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+    o.f6;
+    o.f7;
+    o.f8;
+    o.f9;
+}));
+console.log("read all class with 10 field", measure(() => {
+    const o = createClass10();
+    o.f1;
+    o.f2;
+    o.f3;
+    o.f4;
+    o.f5;
+    o.f6;
+    o.f7;
+    o.f8;
+    o.f9;
+    o.f10;
+}));
+
+function measure(fn) {
+    const times = [];
+    for (let i = 0; i < 10000; i++) {
+        const start = performance.now();
+        fn();
+        const end = performance.now();
+        times.push(end - start);
+    }
+    // 单位 ns
+    const ms = percentile(90, times);
+    return Math.round(ms * 1000 * 1000);
 }
-console.timeEnd("object with 4 field");
-
-console.time("object with 5 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject5();
-}
-console.timeEnd("object with 5 field");
-
-console.time("object with 6 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject6();
-}
-console.timeEnd("object with 6 field");
-
-console.time("object with 7 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject7();
-}
-console.timeEnd("object with 7 field");
-
-console.time("object with 8 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject8();
-}
-console.timeEnd("object with 8 field");
-
-console.time("object with 9 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject9();
-}
-console.timeEnd("object with 9 field");
-
-console.time("object with 10 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createObject10();
-}
-console.timeEnd("object with 10 field");
-
-
-
-
-
-
-
-console.time("class with 1 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass1();
-}
-console.timeEnd("class with 1 field");
-
-console.time("class with 2 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass2();
-}
-console.timeEnd("class with 2 field");
-
-console.time("class with 3 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass3();
-}
-console.timeEnd("class with 3 field");
-
-console.time("class with 4 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass4();
-}
-console.timeEnd("class with 4 field");
-
-console.time("class with 5 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass5();
-}
-console.timeEnd("class with 5 field");
-
-console.time("class with 6 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass6();
-}
-console.timeEnd("class with 6 field");
-
-console.time("class with 7 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass7();
-}
-console.timeEnd("class with 7 field");
-
-console.time("class with 8 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass8();
-}
-console.timeEnd("class with 8 field");
-
-console.time("class with 9 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass9();
-}
-console.timeEnd("class with 9 field");
-
-console.time("class with 10 field");
-for (let i = 0; i < 10000; i++) {
-    let o = createClass10();
-}
-console.timeEnd("class with 10 field");
-
-
-
-
-console.time("class with 4 fields read 2 fields");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass4();
-    c.f1;
-    c.f2;
-}
-console.timeEnd("class with 4 fields read 2 fields");
-
-console.time("class with 4 fields read 3 fields");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass4();
-    c.f1;
-    c.f2;
-    c.f3;
-}
-console.timeEnd("class with 4 fields read 3 fields");
-
-console.time("class with 4 fields read 4 fields");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass4();
-    c.f1;
-    c.f2;
-    c.f3;
-    c.f4;
-}
-console.timeEnd("class with 4 fields read 4 fields");
-
-
-console.time("object with 1 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createObject1();
-    setObject1(c);
-}
-console.timeEnd("object with 1 fields and set");
-
-console.time("object with 2 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createObject2();
-    setObject2(c);
-}
-console.timeEnd("object with 2 fields and set");
-
-console.time("object with 3 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createObject3();
-    setObject3(c);
-}
-console.timeEnd("object with 3 fields and set");
-
-console.time("object with 4 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createObject4();
-    setObject4(c);
-}
-console.timeEnd("object with 4 fields and set");
-
-
-
-console.time("class with 1 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass1();
-    setClass1(c);
-}
-console.timeEnd("class with 1 fields and set");
-
-console.time("class with 2 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass2();
-    setClass2(c);
-}
-console.timeEnd("class with 2 fields and set");
-
-console.time("class with 3 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass3();
-    setClass3(c);
-}
-console.timeEnd("class with 3 fields and set");
-
-console.time("class with 4 fields and set");
-for (let i = 0; i < 10000; i++) {
-    let c = createClass4();
-    setClass4(c);
-}
-console.timeEnd("class with 4 fields and set");
